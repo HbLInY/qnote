92afdd80e571bb5484b1bbd0164823ed898bdd4c
hblinye <a1851yl@aiit.ac.jp>
2021-04-07 21:08:08 +0900

    First Build

---
+++ dist/precache-manifest.f7805f272fc93ea291a1d6d479ef611b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531ee6dd4732c945d12e",
+    "url": "/css/chunk-vendors.c54af802.css"
+  },
+  {
+    "revision": "1f47aad63333cec48ae86bbe2e12cfa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32c6caff13e24a43cd14",
+    "url": "/js/app.00f8aad6.js"
+  },
+  {
+    "revision": "531ee6dd4732c945d12e",
+    "url": "/js/chunk-vendors.3dcd16c4.js"
+  },
+  {
+    "revision": "6b589d239a801d404e2e5db8d3e535ae",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
